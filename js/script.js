5f4624326eb79415ce859d87ebb71eb575fe4d3c
shaikparveen7897-boop <shaik.parveen7897@gmail.com>
2026-07-29 09:19:08 +0000

    javascript

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ alert("welcome to nriit Learning Managemen System")
 let heading =document.getElementById ("welcome");
 heading.innerHTML="welcome future software engineers"
 console.log("heading element:",heading)
+let msg = document.getElementById("message")
+msg.innerHTML ="javascript is fun"
+console.log("Message element: ", msg)
+function showmessage(){
+    alert("welcome to nriit learning management system")
+}
+function changeHeading() {
+    document.getElementById("welcome").innerHTML = "welcome python fullstack Developers"}
+
